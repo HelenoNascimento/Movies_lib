34cd28bf86abe3b2926ab8f02a663fd41c3b4282
Heleno Nascimento <heleno_55@hotmail.com>
2022-07-22 14:18:32 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import { Link, Navigate, useNavigate} from "react-router-dom";
+import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
+
+import './Navbar.css';
+
+const Navbar = () => {
+
+    const [search, setSearch] = useState("");
+    const navegate = useNavigate();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(!search) return
+
+        navegate(`/search?q=${search}`);
+        setSearch("");
+    }
+  return (
+    <div>
+         <nav id="navbar">
+        <h2>
+          <Link to="/"><BiCameraMovie />MoviesLib</Link>
+        </h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)} value={search}/>
+            <button type="submit"><BiSearchAlt2 /></button>
+        </form>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
